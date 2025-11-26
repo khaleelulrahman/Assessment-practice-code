a44045f8e9330a96bb821f253b0c84649e3afaaf
khaleelulrahman <kaleel15593@gmail.com>
2025-11-26 22:46:34 +0530

    initial version

--- src/test/java/AmazonProduct.java
+++ src/test/java/AmazonProduct.java
@@ -24,6 +24,8 @@ public class AmazonProduct {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
         System.out.println("Amazon homepage opened.");
 
         // HANDLE "Continue Shopping"
@@ -58,7 +60,7 @@ public class AmazonProduct {
         WebElement searchProductBox = driver.findElement(By.id("twotabsearchtextbox"));
         searchProductBox.sendKeys("kindle paperwhite");
         System.out.println("Search text entered.");
-        Thread.sleep(3000);
+       // Thread.sleep(3000);
 //
 //        // CLICK PRICE RANGE SLIDER SECTION
 //        System.out.println("Checking for Price Range Slider section...");
@@ -84,7 +86,6 @@ public class AmazonProduct {
 //        driver.findElement(By.xpath("//*[@id=\"p_36/dynamic-picker-0\"]/span/a/span")).click();
 //        System.out.println("upto 150 clicked");
 
-        Thread.sleep(3000);
 
 //        // SLIDER DRAG ELEMENT (fixed Xpath)
 //        System.out.println("Locating price slider handle...");
@@ -92,7 +93,6 @@ public class AmazonProduct {
 //                By.xpath("//*[@id=\"p_36/range-slider_slider-item_lower-bound-slider\"]")
 //        );
 //        slider.click();
-//        Thread.sleep(3000);
 //        System.out.println("location of 100 dollar range"+slider.getLocation());
 //        System.out.println("Slider located successfully.");
 
@@ -101,18 +101,15 @@ public class AmazonProduct {
 //
 //        System.out.println("Dragging slider 50px to the right...");
 //        actions.dragAndDropBy(slider, 16, 76).perform();
-//        Thread.sleep(2000);
 //
 ////        System.out.println("Dragging slider 30px to the left...");
 ////        actions.dragAndDropBy(slider, -30, 0).perform();
 //
 //        System.out.println("Slider movement complete.");
-//        Thread.sleep(2000);
 
 //        //click on 4star and up
 //        driver.findElement(By.xpath("//*[@id=\"p_72/2661618011\"]/span/div/a/i")).click();
 //        System.out.println("4 star and up filter applied");
-        Thread.sleep(2000);
 //get the price of second product
         String productPrice = driver.findElement(By.xpath("(//span[@class='a-color-base'][normalize-space()='$5.94'])[1]")).getText();
         System.out.println("Product Price: " + productPrice);
@@ -120,11 +117,11 @@ public class AmazonProduct {
         //selecting second product from the result
         driver.findElement(By.xpath("//span[contains(text(),'3 Pack Anti-Glare Matte Screen Protector for 7\" Ki')]")).click();
         System.out.println("second product clicked");
-        Thread.sleep(2000);
+
 
         //get the name,price of the product
-        String productName = driver.findElement(By.xpath("//span[contains(text(),'3 Pack Anti-Glare Matte Screen Protector for 7\" Ki')]")).getText();
-        System.out.println(productName);
+        String actualproductName = driver.findElement(By.xpath("//span[contains(text(),'3 Pack Anti-Glare Matte Screen Protector for 7\" Ki')]")).getText();
+        System.out.println(actualproductName);
 
         //clicking on see all buying options
         WebElement seeAllBuyingOptions = wait.until(
@@ -133,30 +130,24 @@ public class AmazonProduct {
         );
         seeAllBuyingOptions.click();
 //
-Thread.sleep(3000);
 
 //compare product name before adding to cart
         String prodNameBeforeCheckoutpage=driver.findElement(By.xpath("//div[@class='a-fixed-left-grid-inner']//h5[@id='aod-asin-title-text']")).getText();
         System.out.println(prodNameBeforeCheckoutpage);
-        Assert.assertEquals(productName,prodNameBeforeCheckoutpage,"Product names do not match before checkout page.");
+        Assert.assertEquals(actualproductName,prodNameBeforeCheckoutpage,"Product names do not match before checkout page.");
 
         //clicking on add to cart
       driver.findElement(By.xpath("//input[@name='submit.addToCart']")).click();
         System.out.println("Product added to cart.");
 
-        Thread.sleep(3000);
 
         //compare price on added to cart page
-        String priceataddedtoCartpage=driver.findElement(By.xpath("//h2[normalize-space()='$5.94']")).getText();
-        System.out.println(priceataddedtoCartpage+"price at added to cart page");
-        Thread.sleep(3000);
-        Assert.assertEquals(productPrice,priceataddedtoCartpage,"Product prices do not match at added to cart page.");
+        String pricebeforecheckout=driver.findElement(By.xpath("//h2[normalize-space()='$5.94']")).getText();
+        System.out.println(pricebeforecheckout+"price at added to cart page");
+        Assert.assertEquals(productPrice,pricebeforecheckout,"Product prices do not match at added to cart page.");
 
         //proceed to checkout
-        WebElement proceedtoCheckout = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.cssSelector("input[value='Proceed to checkout']"))
-        );
+        WebElement proceedtoCheckout = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[value='Proceed to checkout']")));
         proceedtoCheckout.click();
 
         // CLOSE BROWSER
