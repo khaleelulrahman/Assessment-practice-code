15df50838b9a18826bc4eaaef346ffe9d9586a26
khaleelulrahman <kaleel15593@gmail.com>
2025-12-04 16:29:08 +0530

    Adding testNG project

--- src/test/java/Testng.java
+++ src/test/java/Testng.java
@@ -5,12 +5,24 @@ import org.testng.annotations.Test;
 public class Testng {
 
     @Test
+    public void testA()
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.vskills.in/interview-questions/playwright-interview-questions");
+    }
+    @Test
     public void testB()
     {
-WebDriver driver=new ChromeDriver();
-    driver.get("https://www.vskills.in/interview-questions/playwright-interview-questions");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://chatgpt.com/");
     }
-      }
+    @Test
+    public void testc()
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.youtube.com/watch?v=K4MZqwYktUs&list=PLLS0D9-W-1dmoSD9D-h0LxLSytAgyGDSB");
+    }
+}
 
 
 
