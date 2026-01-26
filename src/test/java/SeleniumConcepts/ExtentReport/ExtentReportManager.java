6ea75f99de3011a821d338ce769990b41d93e662
khaleelulrahman <kaleel15593@gmail.com>
2026-01-26 23:34:51 +0530

    screenshot on failed test

---
+++ src/test/java/SeleniumConcepts/ExtentReport/ExtentReportManager.java
@@ -0,0 +1,32 @@
+package SeleniumConcepts.ExtentReport;
+
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+    public class ExtentReportManager {
+
+        public static ExtentReports extent;
+
+        public static ExtentReports getReportInstance() {
+
+            if (extent == null) {
+
+                ExtentSparkReporter reporter =
+                        new ExtentSparkReporter("reports/ExtentReport.html");
+
+                reporter.config().setReportName("Automation Test Report");
+                reporter.config().setDocumentTitle("Selenium Execution Report");
+
+                extent = new ExtentReports();
+                extent.attachReporter(reporter);
+
+                extent.setSystemInfo("Tester", "Khaleel");
+                extent.setSystemInfo("Environment", "QA");
+                extent.setSystemInfo("Browser", "Chrome");
+            }
+            return extent;
+        }
+    }
+
+
